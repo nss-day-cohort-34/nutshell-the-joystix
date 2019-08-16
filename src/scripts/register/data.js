8d5ee1edbbf981021de5131d87fc9692e249de27
Will Wilkinson <40211746+wfw40869@users.noreply.github.com>
2019-08-16 10:21:30 -0500

    Added register user functionality. Next thing to work on is login feature and then render home page.

--- src/scripts/register/data.js
+++ src/scripts/register/data.js
@@ -1,6 +1,21 @@
-const getUsersData = () => {
- return fetch("http://localhost:8088/users")
- .then(response => response.json())
-}
 
-export default getUsersData
+
+
+
+export default {
+    getUsersData: () => {
+        return fetch("http://localhost:8088/users")
+            .then(response => response.json())
+    },
+    addUser: (newUser) => {
+        return fetch("http://localhost:8088/users",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newUser)
+        })
+        .then(response => response.json())
+    }
+
+}
--- src/scripts/register/register.js
+++ src/scripts/register/register.js
@@ -1,4 +1,4 @@
-import getUsersData from "./data.js"
+import API from "./data.js"
 
 const addUser = () => {
     const mainTag = document.querySelector("#main")
@@ -9,16 +9,36 @@ const addUser = () => {
             const emailInput = document.querySelector("#email")
             const usernameInput = document.querySelector("#username")
             const passwordInput = document.querySelector("#password")
-            console.log("firstNameInput: ", firstNameInput.value);
-            console.log("lastNameInput: ", lastNameInput.value);
-            console.log("emailInput: ", emailInput.value);
-            console.log("passwordInput: ", passwordInput.value);
-            console.log("usernameInput: ", usernameInput.value);
-            mainTag.innerHTML = ""
 
-            getUsersData()
+            API.getUsersData()
             .then(parsedUsers => {
-                console.log(parsedUsers)
+                if(parsedUsers.find(user => user.username.toLowerCase() === usernameInput.value.toLowerCase())) {
+                    alert("Username already exists")
+                } else if (parsedUsers.find(user => user.email.toLowerCase() === emailInput.value.toLowerCase())){
+                    alert("Email already exists")
+                } else if(usernameInput.value === "" || emailInput.value === "" || passwordInput.value === "" || firstNameInput.value === "" || lastNameInput.value === "") {
+                    alert("Please fill out all fields")
+                } else if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailInput.value))){
+                    alert("Please enter a valid email address")
+                }
+                else {
+                    mainTag.innerHTML = ""
+                    //Create a new user Object
+                    const newUserObj = {
+                        username: usernameInput.value,
+                        email: emailInput.value,
+                        password: passwordInput.value,
+                        firstName: firstNameInput.value,
+                        lastName: lastNameInput.value
+                    }
+                    // Post it to the Users Array
+                    API.addUser(newUserObj)
+                    .then(user => {
+                        // Set session storage to store logged in user's id
+                        sessionStorage.setItem("activeUser", user.id)
+                    })
+                    // Render Home Page
+                }
             })
         }
     })
