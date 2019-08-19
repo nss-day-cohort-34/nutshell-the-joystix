88a7fd018ba9b4b777646adb3a83d323acde5a12
Kelly Coles <kellycoles1@gmail.com>
2019-08-18 20:03:02 -0500

    added fieldsets, buttons, sections

--- src/scripts/events/factory.js
+++ src/scripts/events/factory.js
@@ -1,21 +1,49 @@
-const newEventObject = (name, date, location) => {
-    return {
-        name: name,
-        date: date,
-        location: location,
-     }
- }
- const createEventHTML = (name, location, date) => {
-    return `
-    <h2 class="name">${name}</h2>
-    <p class="info"> ${location}</p>
-    <p class="info">${date}</p>
+const htmlRep = {
+   createAddEventsButton() {
+      return `
+   <button id="addEvent__button">Add a New Event</button>
+   `
+   },
+
+   createEventInputs() {
+      return `
+   <section id="event__inputs">
+      <legend>Add New Event</legend>
+      <fieldset>
+         <label for="eventName__input">Name:</label>
+         <input type="text" id="eventName__input">
+         <label for="eventDate__input">Date:</label>
+         <input type="date" id="eventDate__input">
+         <label for="eventLocation__input">Location:</label>
+         <input type="text" id="eventLocation__input"></textarea>
+      </fieldset>
+      <button id="addEvent__button">Add Event</button>
+   </section>
+   `
+   },
+
+   newEventObject(name, date, location) {
+      return {
+         name: name,
+         date: date,
+         location: location,
+      }
+   },
+
+   createEventHTML(event) {
+      return `
+    <section id="event__output${event.id}>
+      <h2 class="name">${event.name}</h2>
+      <p class="info">${event.date}</p>
+      <p class="info"> ${event.location}</p>
+      <button class = "editBtn" id="editBtnId--${event.id}">
+      Edit
+      </button>
+      <button class = "deleteBtn" id="deleteBtnId--${event.id}">
+      Delete
+      </button>
+    </section>
     `
- }
- const createEventInputs = () => {
-    return `
-    <input type="text" id="eventName__input">
-    <textarea id="eventLocation__input></textarea>
-    <input type="date" id="eventDate__input>
-    `
- }
+   }
+}
+export default htmlRep
